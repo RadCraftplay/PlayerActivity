267d32c593d9d190852652d13a0b3fea0726501d
Distroir <radcraftplay2@gmail.com>
2019-06-20 12:35:04 +0200

    Added basic config

--- src/main/java/io/github/radcraftplay/playeractivity/PlayerActivity.java
+++ src/main/java/io/github/radcraftplay/playeractivity/PlayerActivity.java
@@ -2,14 +2,38 @@ package io.github.radcraftplay.playeractivity;
 
 import io.github.radcraftplay.playeractivity.commands.executors.PlayersCommandExecutor;
 import io.github.radcraftplay.playeractivity.listeners.PlayerActivityListener;
+import io.github.radcraftplay.playeractivity.player.list.PlayerListSettings;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PlayerActivity extends JavaPlugin {
   @Override
   public void onEnable() {
+    loadConfig();
+
     PlayerConnectionStorage storage = PlayerConnectionStorage.getInstance();
+    PlayerListSettings listSettings = getListSettings();
 
     getServer().getPluginManager().registerEvents(new PlayerActivityListener(storage.connections), this);
     getServer().getPluginCommand("players").setExecutor(new PlayersCommandExecutor(storage.connections));
+  }
+
+  private PlayerListSettings getListSettings() {
+    PlayerListSettings settings = new PlayerListSettings();
+    FileConfiguration config = getConfig();
+
+    settings.setListLength((int)config.get("list.length"));
+    settings.setDisplayOnlinePlayers((boolean)config.get("list.displayOnlinePlayers"));
+
+    return settings;
+  }
+
+  private void loadConfig() {
+    FileConfiguration config = this.getConfig();
+    config.addDefault("list.length", 9);
+    config.addDefault("list.displayOnlinePlayers", true);
+
+    config.options().copyDefaults(true);
+    this.saveConfig();
   }
 }
---
+++ src/main/java/io/github/radcraftplay/playeractivity/player/list/PlayerListSettings.java
@@ -0,0 +1,22 @@
+package io.github.radcraftplay.playeractivity.player.list;
+
+public class PlayerListSettings {
+  private int listLength;
+  private boolean displayOnlinePlayers;
+
+  public int getListLength() {
+    return listLength;
+  }
+
+  public void setListLength(int listLength) {
+    this.listLength = listLength;
+  }
+
+  public boolean getDisplayOnlinePlayers() {
+    return displayOnlinePlayers;
+  }
+
+  public void setDisplayOnlinePlayers(boolean displayOnlinePlayers) {
+    this.displayOnlinePlayers = displayOnlinePlayers;
+  }
+}
